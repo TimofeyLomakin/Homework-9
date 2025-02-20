baf0c42124911cd488cfd65047928d3ed826788e
Lomakin Timofey <timworkey@gmail.com>
2025-02-21 00:16:18 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,89 @@
+package ru.netology
+
+data class Chats(
+    val chatId: Int,
+    val userId: Int,
+    val messages: MutableList<Messages>
+)
+
+data class Messages(
+    val userIdMessage: Int,
+    var textMessage: String,
+    val idMessage: Int,
+    var unread: Boolean
+)
+
+class DirectMessages {
+    val chats = mutableListOf<Chats>()
+
+    fun deleteChat(chatId: Int): Boolean {
+        val findChat = chats.find { it.chatId == chatId } ?: throw IllegalArgumentException("Чат не найден")
+        findChat.messages.clear()
+        chats.remove(findChat)
+        return true
+    }
+
+    fun getAllChats(): List<Chats> {
+        return chats.toList()
+    }
+
+    fun createMessage(chatId: Int, messages: Messages, userId: Int): Boolean {
+
+        val newMessage = Messages(
+            userIdMessage = messages.userIdMessage,
+            textMessage = messages.textMessage,
+            idMessage = messages.idMessage,
+            unread = messages.unread
+        )
+        val createChat = chats.find { it.chatId == chatId }
+        if (createChat == null) {
+            chats.add(
+                Chats(
+                    chatId = chatId,
+                    userId = userId,
+                    messages = mutableListOf(newMessage)
+                )
+            )
+        } else {
+            createChat.messages.add(newMessage)
+
+        }
+        return true
+    }
+
+    fun deleteMessage(chatId: Int, idMessage: Int): Boolean {
+        val findChat = chats.find { it.chatId == chatId } ?: throw IllegalArgumentException("Чат не найден")
+        val findMessage = findChat.messages.find { it.idMessage == idMessage }
+            ?: throw IllegalArgumentException("Сообщение не найдено")
+        findChat.messages.remove(findMessage)
+        return true
+    }
+
+    fun getChatMessages(chatId: Int): List<Messages> {
+        val findChat = chats.find { it.chatId == chatId } ?: throw IllegalArgumentException("Чат не найден")
+        findChat.messages.forEach { it.unread = false }
+        return findChat.messages
+    }
+
+    fun getMessagesToUser(chatId: Int, userIdMessage: Int): List<Messages> {
+        val getChat = chats.find { it.chatId == chatId } ?: throw IllegalArgumentException("Чат не найден")
+        val getMessages = getChat.messages.filter { it.userIdMessage == userIdMessage }
+
+        return getMessages
+    }
+
+    fun getLastMessages(): List<String> {
+        return chats.map { chats ->
+            chats.messages.lastOrNull()?.textMessage ?: "Нет сообщений"
+        }
+    }
+
+    fun getUnreadChatsCount(): Int {
+        return chats.count { chat -> chat.messages.any { !it.unread } }
+    }
+}
+
+
+fun main() {
+
+}
---
+++ src/test/kotlin/DirectMessagesTest.kt
@@ -0,0 +1,117 @@
+import org.junit.Assert.*
+import ru.netology.Chats
+import ru.netology.DirectMessages
+import ru.netology.Messages
+import kotlin.test.Test
+
+class DirectMessagesTest {
+
+    @Test
+    fun deleteChat() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        assertTrue(directMessages.deleteChat(1))
+        assertEquals(0, directMessages.getAllChats().size)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun deleteChatIllegalArgumentException() {
+        val directMessages = DirectMessages()
+        directMessages.deleteChat(1)
+    }
+
+    @Test
+    fun getAllChats() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        assertEquals(1, directMessages.getAllChats().size)
+    }
+
+    @Test
+    fun createMessage() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        val createMessage = directMessages.createMessage(1, Messages(1, "Новое сообщение", 2, true), 1)
+        assertTrue(createMessage)
+        assertEquals(2, directMessages.getChatMessages(1).size)
+    }
+
+    @Test
+    fun createChat() {
+        val directMessages = DirectMessages()
+        val createChat = directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        assertTrue(createChat)
+    }
+
+    @Test
+    fun deleteMessage() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 2, true), 1)
+        assertTrue(directMessages.deleteMessage(1, 2))
+        assertEquals(1, directMessages.getChatMessages(1).size)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun deleteMessageIllegalArgumentExceptionToChat() {
+        val directMessages = DirectMessages()
+        directMessages.deleteMessage(1, 1)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun deleteMessageIllegalArgumentExceptionToMessage() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        directMessages.deleteMessage(1, 2)
+    }
+
+    @Test
+    fun getChatMessages() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение 1", 1, true), 1)
+        directMessages.createMessage(1, Messages(1, "Новое сообщение 2", 2, true), 1)
+        assertEquals(2, directMessages.getChatMessages(1).size)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getChatMessagesIllegalArgumentException() {
+        val directMessages = DirectMessages()
+        directMessages.getChatMessages(1)
+    }
+
+    @Test
+    fun getMessagesToUser() {
+        val directMessages = DirectMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение", 1, true), 1)
+        assertEquals(1, directMessages.getMessagesToUser(1, 1).size)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getMessagesToUserIllegalArgumentException() {
+        val directMessages = DirectMessages()
+        directMessages.getMessagesToUser(1, 1)
+    }
+
+    @Test
+    fun getLastMessages() {
+        val directMessages = DirectMessages()
+        directMessages.getLastMessages()
+        assertEquals(0, directMessages.getLastMessages().size)
+        directMessages.createMessage(1, Messages(1, "Новое сообщение1", 1, true), 1)
+        assertEquals(1, directMessages.getLastMessages().size)
+        directMessages.createMessage(1, Messages(1, "Новое сообщение2", 2, true), 1)
+        assertEquals(1, directMessages.getLastMessages().size)
+        directMessages.createMessage(2, Messages(1, "Новое сообщение3", 1, true), 1)
+        assertEquals(2, directMessages.getLastMessages().size)
+    }
+
+    @Test
+    fun getUnreadChatsCount() {
+        val directMessages = DirectMessages()
+        directMessages.getLastMessages()
+        directMessages.createMessage(1, Messages(1, "Новое сообщение1", 1, true), 1)
+        assertEquals(0, directMessages.getUnreadChatsCount())
+        directMessages.createMessage(1, Messages(1, "Новое сообщение1", 1, false), 1)
+        assertEquals(1, directMessages.getUnreadChatsCount())
+    }
+}
